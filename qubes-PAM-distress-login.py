9eaa94afd2ed56f91640468add0daccab9082cf1
journoSEC <kenrosen@proton.me>
2023-02-19 22:07:06 +0100

    Create qubes-PAM-distress-login.py

---
+++ qubes-PAM-distress-login.py
@@ -0,0 +1,43 @@
+
+import smtplib
+import getpass
+import pam
+
+# Set up the SMTP server
+server = smtplib.SMTP('smtp.protonmail.com', 587)
+server.starttls()
+
+# Prompt the user for their login credentials
+username = input('Enter your username: ')
+password = getpass.getpass('Enter your password: ')
+
+# Set up the PAM service
+service = 'login'
+
+# Authenticate the user
+if pam.authenticate(username, password, service):
+    # Prompt the user for the passphrase
+    passphrase = getpass.getpass('Enter the passphrase: ')
+
+    # Check if the passphrase is correct
+    if passphrase == 'password123':
+        # Send the distress email
+        recipients = ['email1@example.com', 'email2@example.com']
+        subject = 'Distress signal'
+        body = 'I am in need of assistance. Please help.'
+        message = 'Subject: {}\n\n{}'.format(subject, body)
+
+        # Log in to the ProtonMail SMTP server
+        pm_username = 'your-email-address@protonmail.com'
+        pm_password = 'your-protonmail-password'
+        server.login(pm_username, pm_password)
+
+        # Send the email
+        server.sendmail(pm_username, recipients, message)
+
+        # Disconnect from the server
+        server.quit()
+    else:
+        print('Incorrect passphrase')
+else:
+    print('Incorrect username or password')
